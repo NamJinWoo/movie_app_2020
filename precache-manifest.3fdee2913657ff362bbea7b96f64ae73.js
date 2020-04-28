86c4e8a20eb1ca27671e620e1572905f99ec5bd5
NamJinWoo <cjsencks@naver.com>
2020-04-28 16:12:39 +0900

    Updates

---
+++ precache-manifest.3fdee2913657ff362bbea7b96f64ae73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2dc566682d3b4a16fa57908daae96b",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "816787d8e5f7b6bf9624",
+    "url": "/movie_app_2020/static/css/main.d01dcbda.chunk.css"
+  },
+  {
+    "revision": "8df9057e65a5d03cf33d",
+    "url": "/movie_app_2020/static/js/2.f0f7793a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.f0f7793a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816787d8e5f7b6bf9624",
+    "url": "/movie_app_2020/static/js/main.4f9dfc33.chunk.js"
+  },
+  {
+    "revision": "e76fc168c1d893da3cab",
+    "url": "/movie_app_2020/static/js/runtime-main.ea46b0e5.js"
+  }
+]);
